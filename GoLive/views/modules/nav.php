993004cc9a02046270dd1c3626f553724db5cf33
andy10134 <andy1013@hotmail.com>
2020-11-19 14:46:23 -0300

    Preloader agregado

--- GoLive/views/modules/nav.php
+++ GoLive/views/modules/nav.php
@@ -22,7 +22,7 @@
             echo'<a href="ingresar" class="get-started-btn scrollto">Iniciar Sesión</a>
              <a href="registrarse" class="get-started-btn scrollto">Registrarse</a>';  
           }else{
-            echo '<a href="" class="get-started-btn scrollto">'.$_SESSION["username"].'</a>
+            echo '<a href="perfil" class="get-started-btn scrollto">'.$_SESSION["username"].'</a>
             <a href="salir" class="get-started-btn scrollto">Salir</a>';
           }
           ?>
