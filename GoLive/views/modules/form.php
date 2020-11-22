b311eea81bc21084caa4f40acaa11a7f2ce18ceb
andy10134 <andy1013@hotmail.com>
2020-11-22 12:36:56 -0300

    .

--- GoLive/views/modules/form.php
+++ GoLive/views/modules/form.php
@@ -1,4 +1,66 @@
-<form action="" method="POST" enctype="multipart/form-data"/>
+<!-- <form action="" method="POST" enctype="multipart/form-data"/>
 Añadir imagen: <input name="archivo" id="archivo" type="file"/>
 <input type="submit" name="subir" value="Subir imagen"/>
-</form>
+</form> -->
+
+<div class="row">
+   <div class="small-12 medium-2 large-2 columns">
+     <div class="circle">
+       <!-- User Profile Image -->
+
+       <!-- Default Image -->
+       <!-- <i class="fa fa-user fa-5x"></i> -->
+     </div>
+     <div class="p-image">
+       <!-- <i class="fa fa-camera upload-button"></i> -->
+       <i class="icofont-camera upload-button"></i>
+        <input class="file-upload" type="file" accept="image/*"/>
+     </div>
+  </div>
+</div>
+<style>
+body {
+  background-color: #efefef;
+}
+
+.profile-pic {
+    max-width: 200px;
+    max-height: 200px;
+    display: block;
+}
+
+.file-upload {
+    display: none;
+}
+.circle {
+    border-radius: 1000px !important;
+    overflow: hidden;
+    width: 128px;
+    height: 128px;
+    border: 8px solid rgba(255, 255, 255, 0.7);
+    position: absolute;
+    top: 72px;
+}
+img {
+    max-width: 100%;
+    height: auto;
+}
+.p-image {
+  position: absolute;
+  top: 167px;
+  right: 30px;
+  color: #666666;
+  transition: all .3s cubic-bezier(.175, .885, .32, 1.275);
+}
+.p-image:hover {
+  transition: all .3s cubic-bezier(.175, .885, .32, 1.275);
+}
+.upload-button {
+  font-size: 1.2em;
+}
+
+.upload-button:hover {
+  transition: all .3s cubic-bezier(.175, .885, .32, 1.275);
+  color: #999;
+}
+</style>
--- GoLive/views/template.php
+++ GoLive/views/template.php
@@ -184,6 +184,28 @@
 					$('#submitButton').prop('disabled', true);
 				}
 			});
+
+			    
+			var readURL = function(input) {
+				if (input.files && input.files[0]) {
+					var reader = new FileReader();
+
+					reader.onload = function (e) {
+						$('.profile-pic').attr('src', e.target.result);
+					}
+
+					reader.readAsDataURL(input.files[0]);
+				}
+			}
+
+
+			$(".file-upload").on('change', function(){
+				readURL(this);
+			});
+
+			$(".upload-button").on('click', function() {
+			$(".file-upload").click();
+			});
 			
 		});
 	</script>
